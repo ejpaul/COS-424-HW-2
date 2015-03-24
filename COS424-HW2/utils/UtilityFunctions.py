f5472675a7939ce0eb40d69bdcdcdb0e848d8974
Elizabeth J. Paul <epaul@davisson.princeton.edu>
2015-03-24 11:23:07 -0400

    Added command line parsing to UtilityFunctions.py.

--- COS424-HW2/utils/UtilityFunctions.py
+++ COS424-HW2/utils/UtilityFunctions.py
@@ -4,11 +4,8 @@ Created on Mar 23, 2015
 @author: jonathanshor
 '''
 import nltk, numpy
-import getopt, sys, time
-
-#BE SURE TO UPDATE TO PATH TO YOUR bed FILES
-YOUR_PATH = "/Users/jonathanshor/Google Drive/Edu/2015 S/COS424 - Interacting with Data/COS424-HW2/base_files/"
-
+import sys, time
+from optparse import OptionParser
 
 TRAIN_FNAME = "intersected_final_chr1_cutoff_20_train_revised.bed"
 SAMPLE_FNAME = "intersected_final_chr1_cutoff_20_sample.bed"
@@ -16,28 +13,19 @@ TEST_FNAME = "intersected_final_chr1_cutoff_20_test.bed"
 
 def read_bed_dat(myfile):
     return numpy.loadtxt(myfile, dtype="string", delimiter="\t")
-    
 
-#Currently does not acknowledge command line args; set all paths via CONSTANTS above
 def main(argv):
-#     path = ''
-#     
-#     try:
-#         opts, args = getopt.getopt(argv, "p:", ["path="])
-#     except getopt.GetoptError:
-#         print 'Bad args'
-#         sys.exit(2)
-#     for opt, arg in opts:
-#         if opt in ('-p', '--path'):
-#             path = arg
-    path = YOUR_PATH
+	parser = OptionParser()
+	parser.add_option("-p", "--path", dest="path", help='read bed data fom PATH', metavar='PATH')
+	(options, args) = parser.parse_args()     
+	path = options.path
+	print "PATH = " + path
+	trains = read_bed_dat(path + TRAIN_FNAME)
+	sample = read_bed_dat(path + SAMPLE_FNAME)
     
-    trains = read_bed_dat(path + TRAIN_FNAME)
-    sample = read_bed_dat(path + SAMPLE_FNAME)
-    
-    print "Sample[0] =%s" % sample[0]
-    print "Trains[0] =%s" % trains[0]
-    print "len(Trains) =%s" % len(trains)
+	print "Sample[0] =%s" % sample[0]
+	print "Trains[0] =%s" % trains[0]
+	print "len(Trains) =%s" % len(trains)
+
 if __name__ == '__main__':
     main(sys.argv[1:])
-#    main("-p /Users/jonathanshor/Google Drive/Edu/2015 S/COS424 - Interacting with Data/COS424-HW2/base_files/")
