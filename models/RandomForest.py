b58e5fcea44b8b6b3518361ae12b59971df2d432
epaul626 <epaul@princeton.edu>
2015-04-01 10:20:30 -0400

    Updated RandomForest with RMSE and r2

--- models/RandomForest.py
+++ models/RandomForest.py
@@ -4,7 +4,7 @@ created on Mar 29, 2015
 @author: epaul626
 '''
 import numpy as np
-import sys
+import sys, time
 from optparse import OptionParser
 from sklearn.ensemble import RandomForestRegressor
 sys.path.append('../utils/')
@@ -16,12 +16,11 @@ from FillData import *
 # Beta is an array of shape (# of CpG sites, nsamples)
 # Sites is an array of shape (# of CpG sites, 2) corresponding to 
 # start and ending bp of site
-def feat_neighbors(sites, train_beta, sample):
+def feat_neighbors(sites, train_beta, sample, test):
 	X = np.zeros((len(train_beta), 38))
-	Y = np.zeros((len(train_beta)))
-	# Feature 1: CpG start site
-	X[:,0] = sites[:,0]
 	for i in range(0, len(train_beta)):
+		# Feature 1: CpG start site
+		X[i,0] = sites[i]
                 # Find 2 nearest neighbors to site i
                 (indices,distance)  = find_neighbors(sites, i)
                 index1 = indices[0]
@@ -37,18 +36,30 @@ def feat_neighbors(sites, train_beta, sample):
 			X[i, 4] = distance[1]
 			# Features 6-38: 33 sample beta values at CpG site
 			X[i, 5+j] = train_beta[i, j]
-	return (X, Y)
+	# Predict on feature set not on 450k chip
+	Xstar = X[sample['450k']==0]
+    	gTruth = test['Beta'][sample['450k']==0]
+    	Xstar = Xstar[~np.isnan(gTruth)]
+   	gTruth = gTruth[~np.isnan(gTruth)]
+
+        # Only train on non-NaN values of Y
+        X = X[~np.isnan(sample['Beta'])]
+        Y = sample['Beta'][~np.isnan(sample['Beta'])]
+	return (X, Y, Xstar, gTruth)
 
 # Find nearest 2 sites to current CpG site
 # Returns list of indices of 2 nearest neighbors
 # Where the index in 'sites' is i + (index1, index2)
 def find_neighbors(sites, i):
-	start_curr = sites[i, 0]
-	end_curr = sites[i, 1]
-	d_l1 = start_curr - sites[i-1, 1]
-	d_l2 = start_curr - sites[i-2, 1]
-	d_r1 = sites[i-1, 0] - end_curr
-	d_r2 = sites[i-2, 0] - end_curr
+	curr = sites[i]
+	if (i > 0):
+		d_l1 = curr - sites[i-1]
+	if (i > 1):
+		d_l2 = curr - sites[i-2]
+	if (i < len(sites)-1):
+		d_r1 = sites[i+1] - curr
+	if (i < len(sites)-2):
+		d_r2 = sites[i+2] - curr
 	# Look to left
 	if (i == 0):
 		return ([1, 2], [d_r1, d_r2])
@@ -75,17 +86,33 @@ def main(argv):
 	parser.add_option("-p", "--path", dest="path", help='read bed data fom PATH', metavar='PATH')
 	(options, args) = parser.parse_args()
 	path = options.path
+	start_time = time.time()
+
+# Read in data
         train = read_bed_dat_train(path)
 	sample = read_bed_dat_sample(path)
-        sites = np.transpose(np.array((train['Start'], train['End'])))
+	test = read_bed_dat_test(path)
+	sites = train['Start']
 # Fill in NaNs in training with mean over 33 samples in training bed
         train_beta = fill_mean(train['Beta'])
 # Produce feature array 'X' and vector of beta values 'Y'
-	(X, Y) = feat_neighbors(sites.copy(), train_beta.copy(), sample.copy())
+# Produce feature array 'X*' to predict on and ground truth beta values 'Y*'
+	(X, Y, Xstar, Ystar) = feat_neighbors(sites.copy(), train_beta.copy(), sample.copy(), test.copy())
+
 # Initialize regressor with default parameters
+	start_rf = time.time()
 	model = RandomForestRegressor()
 # Fit regressor using training data
 	model.fit(X, Y)
+# Predict on Xstar values 
+	Yhat = model.predict(Xstar)
+# Calculate r2 and RMSE
+        (r2, RMSE) = calc_r2_RMSE(Yhat, Ystar)
+
+	print "Runtime: %f" % (time.time()-start_time)
+	print "RandomForest Runtime: %f" % (time.time()-start_rf)
+	print "r2 : %f" % (r2)
+	print "RMSE: %f" % (RMSE)
 
 if __name__ == '__main__':
     main(sys.argv[1:])
