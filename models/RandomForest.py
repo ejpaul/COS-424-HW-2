726fcca3ab0fedbf1f7144e3926284164707ca8f
epaul626 <epaul@princeton.edu>
2015-04-02 15:23:34 -0400

    Cleaned up code. Changed some comments.

--- models/RandomForest.py
+++ models/RandomForest.py
@@ -12,38 +12,6 @@ sys.path.append('../utils/')
 from UtilityFunctions import *
 from FillData import *
 
-# Treats each CpG site at each sample as its own sample
-# Rather than as features for a given CpG site
-def feat_all_samples(sites, train_beta, sample, test):
-# 5 X features
-# Feature 1: CpG start site
-# Feature 2: distance to neighbor 1
-# Feature 3: beta at neighbor 1
-# Feature 4: distance to neighbor 2
-# Feature 5: beta at neighbor 2
-# Y: Beta value at sample site
-	X = sites_beta_to_feat(sites, train_beta)
-	Y = sites_beta_to_feat(sites, train_beta, True)
-	# Predict on feature set not on 450k chip
-	Xstar = sample[sample['450k']==0]
-	gTruth = test[sample['450k']==0]
-	Xstar = sites_beta_to_feat(Xstar['Start'], Xstar['Beta'])
-	Xstar = Xstar[~np.isnan(gTruth)]
-	gTruth = gTruth[~np.isnan(gTruth)]
-	gTruth = gTruth[~np.isnan(Xstar[:,2])]
-	gTruth = gTruth[~np.isnan(Xstar[:,4])]
-	Xstar = Xstar[~np.isnan(Xstar[:,2])]
-	Xstar = Xstar[~np.isnan(Xstar[:,4])]
-
-	# Only train on non-NaN values of X & Y
-	X = X[~np.isnan(Y)]
-	Y = Y[~np.isnan(Y)]
-	Y = Y[~np.isnan(X[:,2])]
-	Y = Y[~np.isnan(X[:,4])]
-	X = X[~np.isnan(X[:,2])]
-	X = X[~np.isnan(X[:,4])]
-	return (X, Y, Xstar, gTruth)
-
 # Produces 'X' feature array of nearest neighbor beta values that will be fed
 # into regressor
 # Beta is an array of shape (# of CpG sites, nsamples)
@@ -160,20 +128,20 @@ def main(argv):
 	start_time = time.time()
 
 	# Read in data on islands
-        island_start = time.time()
         train = read_bed_dat_train(path, Island=True)
         sample = read_bed_dat_sample(path, Island=True)
         test = read_bed_dat_test(path, Island=True)
         sites = train['Start']
 # Fill in NaNs in training with mean over 33 samples in training bed
-        #train_beta = fill_neighbors(sites, train['Beta'], 10)
+        train_beta = fill_neighbors(sites, train['Beta'], 10)
 	#train_beta = fill_rand(train['Beta'])
-	train_beta = fill_mean(train['Beta'])
+	#train_beta = fill_mean(train['Beta'])
 # Produce feature array 'X' and vector of beta values 'Y'
 # Produce feature array 'X*' to predict on and ground truth beta values 'Y*'
         (X, Y, Xstar, Ystar) = feat_neighbors(sites.copy(), train_beta.copy(), sample.copy(), test.copy())
+	island_start = time.time()
         # Initialize regressor with default parameters
-        model = RandomForestRegressor()
+        model = RandomForestRegressor(oob_score=True)
 # Fit regressor using training data
         model.fit(X, Y)
 # Predict on Xstar values 
@@ -183,63 +151,65 @@ def main(argv):
         print "Intersects with CGIs"
         print "Runtime: %f" % (time.time()-island_start)
         print "RandomForest Runtime: %f" % (time.time()-start_time)
+	print str(model.feature_importances_)
+	print "oob: " + str(model.oob_score_)
         print "r2 : %f" % (r2)
         print "RMSE: %f" % (RMSE)
 
-# use island feature set
-        island_start = time.time()
+# Island feature set
         train = read_bed_dat_train(path, addIsland=True)
         sample = read_bed_dat_sample(path, addIsland=True)
         test = read_bed_dat_test(path, addIsland=True)
         sites = train['Start']
-# Fill in NaNs in training with mean over 33 samples in training bed
-        #train_beta = fill_neighbors(sites, train['Beta'], 10)
+# Fill in NaNs in training set
+        train_beta = fill_neighbors(sites, train['Beta'], 10)
 	#train_beta = fill_rand(train['Beta'])
-	train_beta = fill_mean(train['Beta'])
+	#train_beta = fill_mean(train['Beta'])
 	train_island = train['Island']
 # Produce feature array 'X' and vector of beta values 'Y'
 # Produce feature array 'X*' to predict on and ground truth beta values 'Y*'
 	(X, Y, Xstar, Ystar) = feat_neighbors_islands(sites.copy(), train_island.copy(), train_beta.copy(), sample.copy(), test.copy())
-        # Initialize regressor with default parameters
-        model = RandomForestRegressor()
+
+	island_start = time.time()
 # Fit regressor using training data
         model.fit(X, Y)
 # Predict on Xstar values 
         Yhat = model.predict(Xstar)
-# Calculate r2 and RMSE
+# Calculate metrics
         (r2, RMSE) = calc_r2_RMSE(Yhat, Ystar)
         print "Island feature set"
         print "Runtime: %f" % (time.time()-island_start)
         print "RandomForest Runtime: %f" % (time.time()-start_time)
+	print str(model.feature_importances_)
         print "r2 : %f" % (r2)
+	print "oob: " + str(model.oob_score_)
         print "RMSE: %f" % (RMSE)
 
-
-# Read in data
+# Standard feature set - not including CGI boolean
         train = read_bed_dat_train(path)
 	sample = read_bed_dat_sample(path)
 	test = read_bed_dat_test(path)
 	sites = train['Start']
-# Fill in NaNs in training with mean over 33 samples in training bed
-	#train_beta = fill_neighbors(sites, train['Beta'], 10)
+# Fill in NaNs in training set
+	train_beta = fill_neighbors(sites, train['Beta'], 10)
 	#train_beta = fill_rand(train['Beta'])
-	train_beta = fill_mean(train['Beta'])
+	#train_beta = fill_mean(train['Beta'])
 # Produce feature array 'X' and vector of beta values 'Y'
 # Produce feature array 'X*' to predict on and ground truth beta values 'Y*'
 	(X, Y, Xstar, Ystar) = feat_neighbors(sites.copy(), train_beta.copy(), sample.copy(), test.copy())
-# Initialize regressor with default parameters
 	start_rf = time.time()
-	model = RandomForestRegressor()
 # Fit regressor using training data
 	model.fit(X, Y)
 # Predict on Xstar values 
 	Yhat = model.predict(Xstar)
-# Calculate r2 and RMSE
+# Calculate metrics
         (r2, RMSE) = calc_r2_RMSE(Yhat, Ystar)
 	print "Standard Feature Set"
 	print "Runtime: %f" % (time.time()-start_time)
 	print "RandomForest Runtime: %f" % (time.time()-start_rf)
+	print str(model.feature_importances_)
 	print "r2 : %f" % (r2)
+	print "oob: " + str(model.oob_score_)
 	print "RMSE: %f" % (RMSE)
 
 if __name__ == '__main__':
